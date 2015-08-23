36c9fd70c6f8cc6a94e2f3e9fe775e65f4c3f3e0
nsavagejvm <nscode1@gmail.com>
2015-08-23 04:24:44 +0300

    commit

--- Pig_Analysis.py
+++ Pig_Analysis.py
@@ -76,6 +76,59 @@ df2 = pHandle[pHandle['ip'].isin(values)]
 df2
 
 
+%%writefile preprocess1.pig
+DEFINE preprocess( ) returns data {
+    
+       tmp = LOAD '/root/data_1' using PigStorage(',')
+    
+        as ( time: chararray, ip: chararray, country: chararray, status: chararray);
+    
+      $data = filter tmp by country=='US';
+    
+};
+
+data = preprocess();
+
+STORE data INTO '/root/output';
+
+
+copyToLocal /root/output /root/output
+
+
+%%bash --err pig_out --bg 
+pig -f preprocess1.pig
+
+
+while True:
+    line = pig_out.readline()
+    if not line: 
+        break
+    sys.stdout.write("%s" % line)
+    sys.stdout.flush()
+
+def read_dataset(cols):
+  fhandle = open('/root/output/part-m-00000', 'r')
+  pieces = []
+  pieces.append(pd.read_csv(fhandle, names=cols, dtype=None,header=None, delimiter="\t"))
+  fhandle.close()
+  return pd.concat(pieces, ignore_index=True)
+
+
+cols = ['time', 'ip', 'country', 'status'];
+pHandle = read_dataset( cols)
+
+pHandle.shape
+
+values = ['153.222.218.103', '149.103.25.161']
+df2 = pHandle[pHandle['ip'].isin(values)]
+df2
+
+
+
+
+
+
+
 
 
 
