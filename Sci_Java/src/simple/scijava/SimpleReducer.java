1fb7cf8102495d1597be2da6dd48d69a7b07bd19
nsavagejvm <nsavage@fxcrypto.com>
2015-08-28 01:53:12 +0300

    update

---
+++ Sci_Java/src/simple/scijava/SimpleReducer.java
@@ -0,0 +1,16 @@
+package simple.scijava;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SimpleReducer extends Reducer<Text, Text, Text, Text> {
+
+  public void reduce(Text key,  Iterator<Text> values, Context context)  throws IOException, InterruptedException {
+	    
+	  String temperature = values.next().toString();
+        context.write(key, new Text(temperature));
+
+  }
+}
